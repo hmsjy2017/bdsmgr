d20cb50d197d44b716d7938db753b4945f936b75
hmsjy2017 <i@iamsjy.com>
2023-07-11 19:32:02 +0800

    fix bugs

--- bds.sh
+++ bds.sh
@@ -5,6 +5,7 @@ install_bds() {
     pkg install proot wget jq -y
 
     # 创建 BDS 文件夹
+    cd ~
     mkdir BDS && cd BDS
 
     # 下载 Ubuntu 镜像
@@ -15,18 +16,18 @@ install_bds() {
     # 解压 Ubuntu 镜像
     echo "正在解压 Ubuntu 镜像..."
     proot --link2symlink tar -xvJf ubuntu-rootfs.tar.xz --exclude="dev"||:
-    rm ubuntu-rootfs.tar.gz
+    rm ~/BDS/ubuntu-rootfs/ubuntu-rootfs.tar.gz
 
     # 下载启动脚本并赋予执行权限
     cd ..
-    https://raw.gitmirror.com/hmsjy2017/bdsmgr/main/start-ubuntu.sh
+    wget https://raw.gitmirror.com/hmsjy2017/bdsmgr/main/start-ubuntu.sh
     chmod +x start-ubuntu.sh
 
     echo "Ubuntu 容器已安装完成"
 
     # 在容器中下载并安装Minecraft基岩版
     echo "正在安装 BDS..."
-    cd ubuntu-rootfs/root/
+    cd ~/BDS/ubuntu-rootfs/root/
     mkdir mc && cd mc
     LATEST_VERSION=`curl -s https://raw.gitmirror.com/Bedrock-OSS/BDS-Versions/main/versions.json | jq -r '.linux.stable'`
     echo "Linux BDS 最新版： [${LATEST_VERSION}]"
@@ -45,7 +46,7 @@ run_bds() {
     if [ ! -d "$HOME/BDS/ubuntu-rootfs" ]; then
         echo "请先安装 BDS。"
     else
-        cd $HOME/BDS
+        cd ~/BDS
         ./start-ubuntu.sh
     fi
     exit
@@ -56,13 +57,13 @@ update_bds() {
         echo "请先安装 BDS。"
     else
         echo "正在备份游戏数据..."
-        cd $HOME/BDS/ubuntu-rootfs/root/mc
-        tar -czvf ../bds-backup.tar.gz allowlist.json Dedicated_server.txt permissions.json server.properties worlds/
+        cd ~/BDS/ubuntu-rootfs/root/mc
+        tar -czvf ../bds-backup.tar.gz allowlist.json Dedicated_Server.txt permissions.json server.properties worlds/
         echo "正在删除旧版 BDS..."
         rm -rf *
-        LATEST_VERSION=`curl -s https://raw.githubusercontent.com/Bedrock-OSS/BDS-Versions/main/versions.json | jq -r '.linux.stable'`
+        LATEST_VERSION=`curl -s https://raw.gitmirror.com/Bedrock-OSS/BDS-Versions/main/versions.json | jq -r '.linux.stable'`
         echo "Linux BDS 最新版：[${LATEST_VERSION}]"
-        DOWNLOAD_URL=`curl -s https://raw.githubusercontent.com/Bedrock-OSS/BDS-Versions/main/linux/${LATEST_VERSION}.json | jq -r '.download_url'`
+        DOWNLOAD_URL=`curl -s https://raw.gitmirror.com/Bedrock-OSS/BDS-Versions/main/linux/${LATEST_VERSION}.json | jq -r '.download_url'`
         echo "正在下载最新版 BDS 压缩包..."
         wget ${DOWNLOAD_URL}
         unzip bedrock-server-${LATEST_VERSION}.zip
@@ -79,10 +80,10 @@ uninstall_bds() {
     read -p "你确定要卸载 BDS 吗？(y/n): " confirm
     if [ "$confirm" = "y" ] || [ "$confirm" = "Y" ]; then
         # 删除 Ubuntu 根文件系统
-        rm -rf $HOME/BDS/ubuntu-rootfs
+        rm -rf ~/BDS/ubuntu-rootfs
         # 删除启动脚本
-        rm $HOME/BDS/start-ubuntu.sh
-        rm -rf $HOME/BDS
+        rm ~/BDS/start-ubuntu.sh
+        rm -rf ~/BDS
         echo "BDS 已成功卸载。"
     else
         echo "取消卸载操作。"
@@ -95,6 +96,7 @@ while true; do
     clear
     printf "\nMinecraft 基岩版服务端（BDS）管理脚本\n"
     printf "（此脚本仅适用于 Termux）\n"
+    printf "https://github.com/hmsjy2017/bdsmgr\n\n"
     echo "请选择操作："
     echo "1. 安装 BDS"
     echo "2. 运行 BDS"
